b0236b55ca8726d99089197ddc5d00a54c8f322f
eroux <elie.roux@telecom-bretagne.eu>
2021-11-27 10:36:21 +0100

    first almost working version

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# kvp-phonserv
-small python http API server for KVP phonetics
+# Phonetic API server for KVP
+
+Small python http API server for KVP (Khyentse Vision Project) phonetics
+
+
+## run
+
+```sh
+$ FLASK_APP=server flask run
+```
+
+## test
+
+```sh
+$ curl 'http://localhost:5000/segment' -d 'str=གང་གི་བློ་གྲོས་'
+```
---
+++ server.py
@@ -0,0 +1,86 @@
+from flask import Flask, json, request
+from botok import Text
+import sys
+sys.path.append('../bophono')
+import bophono
+from botok import Text, WordTokenizer
+
+api = Flask("KVP")
+
+options_fastidious = {
+  'weakAspirationChar': '3',
+  'aspirateLowTones': True,
+  'prefixStrategy': 'always',
+  'aiAffixchar': 'ː',
+  'hightonechar':'̄',
+  'lowtonechar':'̱',
+  'nasalchar': '',
+  'stopSDMode': "eow",
+  'eatP': False,
+  'useUnreleasedStops': True,
+  'eatK': False,
+  'syllablesepchar': ''
+}
+
+WT = WordTokenizer()
+PHON_KVP = bophono.UnicodeToApi(schema="KVP", options = {})
+PHON_API = bophono.UnicodeToApi(schema="MST", options = options_fastidious)
+
+def botok_tokenizer(in_str):
+    return WT.tokenize(in_str)
+
+def botok_modifier(tokens):
+    op = []
+    for t in tokens:
+        op_token = {
+            'start': t.start,
+            'end': t.start + t.len,
+            'type': t.chunk_type
+        }
+        op.append(op_token)
+    return op
+
+def segment(in_str):
+    try:
+        t = Text(in_str, tok_params={'profile': 'GMD'})
+        tokens = t.custom_pipeline('dummy', open_pecha_tokenizer, botok_modifier, 'dummy')
+    except:
+        print("botok failed to segment "+in_str)
+        return in_str
+    res = ''
+    first = True
+    for token in tokens:
+       if not first:
+           res += " "
+       first = False
+       res += multisylstr[token['start']:token['end']]
+    return res
+
+def add_phono(in_str, res):
+    words = in_str.split()
+    res_kvp = ""
+    res_api = ""
+    for word in words:
+        res_kvp += PHON_KVP.get_api(word)+'  '
+        res_api += PHON_API.get_api(word)+'  '
+    res["kvp"] = res_kvp
+    res["api"] = res_api
+
+@api.route('/segment', methods=['POST'])
+def segment_and_phon():
+  in_str = request.form['str']
+  seg = segment(in_str)
+  res = { "segmented" : seg }
+  add_phono(seg, res)
+  return json.dumps(res, ensure_ascii=False)
+
+@api.route('/api', methods=['POST'])
+def phon():
+  in_str = request.form['str']
+  res = {}
+  add_phono(in_str, res)
+  return json.dumps(res, ensure_ascii=False)
+
+
+if __name__ == '__main__':
+    api.run() 
